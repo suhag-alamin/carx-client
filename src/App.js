498558a83eb34bfc55fd1d3d3483e55e5dce7f1c
Suhag Al Amin <developersuhag@gmail.com>
2021-11-10 00:47:36 +0600

    install needed package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/oswald/500.css";
+import "@fontsource/oswald/700.css";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline></CssBaseline>
     </div>
   );
 }
