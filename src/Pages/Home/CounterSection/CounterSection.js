bea19cb8101c05374599918ec6db63a1aaaa324c
Suhag Al Amin <developersuhag@gmail.com>
2021-11-11 01:45:46 +0600

    counter

---
+++ src/Pages/Home/CounterSection/CounterSection.js
@@ -0,0 +1,90 @@
+import { Container, Grid, Paper, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import StoreIcon from "@mui/icons-material/Store";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import CountUp from "react-countup";
+
+// counter section
+
+const CounterSection = () => {
+  return (
+    <Container sx={{ py: 6 }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 2, sm: 8, md: 12 }}
+        >
+          <Grid item xs={2} sm={4} md={3}>
+            <Paper sx={{ p: 2, textAlign: "center" }}>
+              <EmojiEmotionsIcon sx={{ fontSize: 30 }} color="primary" />
+              <Typography
+                color="primary"
+                variant="h4"
+                sx={{ fontWeight: 700, my: 1 }}
+              >
+                <CountUp duration={2} end={13000} />
+              </Typography>
+              <Typography color="secondary" variant="subtitle1">
+                HAPPY CUSTOMERS
+              </Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={2} sm={4} md={3}>
+            <Paper sx={{ p: 2, textAlign: "center" }}>
+              <DirectionsCarIcon sx={{ fontSize: 30 }} color="primary" />
+              <Typography
+                color="primary"
+                variant="h4"
+                sx={{ fontWeight: 700, my: 1 }}
+              >
+                <CountUp duration={2} end={300} />
+              </Typography>
+              <Typography color="secondary" variant="subtitle1">
+                CARS IN STOCK
+              </Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={2} sm={4} md={3}>
+            <Paper sx={{ p: 2, textAlign: "center" }}>
+              <StoreIcon sx={{ fontSize: 30 }} color="primary" />
+              <Typography
+                color="primary"
+                variant="h4"
+                sx={{ fontWeight: 700, my: 1 }}
+              >
+                <CountUp duration={2} end={10} />
+              </Typography>
+              <Typography color="secondary" variant="subtitle1">
+                SHOWROOMS
+              </Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={2} sm={4} md={3}>
+            <Paper sx={{ p: 2, textAlign: "center" }}>
+              <AutoAwesomeIcon sx={{ fontSize: 30 }} color="primary" />
+              <Typography
+                color="primary"
+                variant="h4"
+                sx={{ fontWeight: 700, my: 1 }}
+              >
+                <CountUp duration={2} end={50} />
+              </Typography>
+              <Typography color="secondary" variant="subtitle1">
+                AWWARDS
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+export default CounterSection;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import useDocumentTitle from "../../../hooks/useDocumentTitle";
 import Footer from "../../Shared/Footer/Footer";
 import Navigation from "../../Shared/Navigation/Navigation";
 import Banner from "../Banner/Banner";
+import CounterSection from "../CounterSection/CounterSection";
 import FeaturedCars from "../FeaturedCars/FeaturedCars";
 import Reviews from "../Reviews/Reviews";
 import RunningCar from "../RunningCar/RunningCar";
@@ -21,6 +22,8 @@ const Home = () => {
       <RunningCar />
       {/* featured cars section  */}
       <FeaturedCars />
+      {/* counter section  */}
+      <CounterSection />
       {/* review section  */}
       <Reviews />
       {/* footer  */}
