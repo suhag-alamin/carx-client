4ddc30fed00dd489634f8675ca978e22e1c2c2d2
Suhag Al Amin <developersuhag@gmail.com>
2021-11-11 01:21:08 +0600

    car running section

--- src/Pages/Home/RunningCar/RunningCar.js
+++ src/Pages/Home/RunningCar/RunningCar.js
@@ -1,20 +1,15 @@
 import { Container, Divider, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import car from "../../../images/car.jpg";
+import car from "../../../images/car.png";
 import wheel from "../../../images/car-tier.png";
 import "./RunningCar.css";
 
 const RunningCar = () => {
-  const mainNav = document.querySelector(".main-nav");
   window.onscroll = function () {
     windowScroll();
   };
   function windowScroll() {
-    // mainNav?.classList.toggle(
-    //   "in-view",
-    //   document.documentElement.scrollTop > 50
-    // );
     if (
       document.body.scrollTop > 200 ||
       document.documentElement.scrollTop > 200
@@ -32,31 +27,38 @@ const RunningCar = () => {
         rowSpacing={1}
         columnSpacing={{ xs: 1, sm: 2, md: 3 }}
       >
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={8} lg={6}>
           <Box id="main-nav" className="running-car main-nav">
             <div className="runnig-box">
-              <img className="car " src={car} alt="" />
+              <img
+                style={{ width: "100%" }}
+                className="car "
+                src={car}
+                alt=""
+              />
               <img className="wheel" src={wheel} alt="" />
             </div>
           </Box>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography
-            sx={{ pb: 3, fontWeight: 700 }}
-            color="secondary"
-            variant="h4"
-          >
-            OUR FLEET YOUR FLEET
-          </Typography>
-          <Divider />
-          <Typography sx={{ pt: 2 }} paragraph color="text.secondary">
-            Assertively iterate enterprise-wide portals through synergistic
-            products. Efficiently build adaptive schemas after transparent
-            quality vectors. Phosfluorescently optimize competitive resources
-            after extensive convergence. Rapidiously optimize high-quality
-            meta-services via distributed architectures. Credibly deliver
-            24/365.
-          </Typography>
+        <Grid item xs={12} md={4} lg={6}>
+          <Box sx={{ px: 2 }}>
+            <Typography
+              sx={{ pb: 3, fontWeight: 700 }}
+              color="secondary"
+              variant="h4"
+            >
+              OUR FLEET YOUR FLEET
+            </Typography>
+            <Divider />
+            <Typography sx={{ pt: 2 }} paragraph color="text.secondary">
+              Assertively iterate enterprise-wide portals through synergistic
+              products. Efficiently build adaptive schemas after transparent
+              quality vectors. Phosfluorescently optimize competitive resources
+              after extensive convergence. Rapidiously optimize high-quality
+              meta-services via distributed architectures. Credibly deliver
+              24/365.
+            </Typography>
+          </Box>
         </Grid>
       </Grid>
     </Box>
