3881da0f4a96af6d8192084a5c495e46f932e3a5
suhag-alamin <developersuhag@gmail.com>
2023-11-14 18:37:32 +0600

    upgrade to vite from cra

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import AuthProvider from "./context/AuthProvider";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <AuthProvider>
+//       <App />
+//     </AuthProvider>
+//   </React.StrictMode>
+// );
+
+// ! need to update all dependencies
