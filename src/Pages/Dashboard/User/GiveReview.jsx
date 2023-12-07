a29aedf058cc7a9ef26f381f7a86e7beb6977e0b
suhag-alamin <developersuhag@gmail.com>
2023-12-07 17:55:39 +0600

    update give review

--- src/Pages/Dashboard/User/GiveReview.jsx
+++ src/Pages/Dashboard/User/GiveReview.jsx
@@ -1,3 +1,5 @@
+import Form from "@/components/Forms/Form";
+import FormTextField from "@/components/Forms/FormTextField";
 import { usePostReviewMutation } from "@/redux/features/review/reviewApi";
 import {
   Button,
@@ -6,12 +8,10 @@ import {
   Divider,
   Grid,
   Rating,
-  TextField,
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
@@ -22,20 +22,12 @@ const GiveReview = () => {
 
   const [star, setStar] = useState(2);
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
-
   const onSubmit = (data) => {
     if (data) {
       data.rating = star;
       data.user = user._id;
 
       postReview(data);
-      reset();
     }
   };
 
@@ -62,71 +54,64 @@ const GiveReview = () => {
             marginX: "auto",
           }}
         >
-          <Box
-            component="form"
-            onSubmit={handleSubmit(onSubmit)}
-            sx={{ mt: 3 }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  type="text"
-                  fullWidth
-                  label="Review"
-                  required
-                  multiline
-                  {...register("review", { required: true })}
-                />
-                {errors.review && (
-                  <span className="error">Review is required</span>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                <Typography
-                  color="secondary"
-                  sx={{ mb: 1 }}
-                  variant="subtitle1"
-                >
-                  Give us your rating
-                </Typography>
-                <Rating
-                  precision={0.5}
-                  size="large"
-                  value={star}
-                  required
-                  onChange={(event, newValue) => {
-                    setStar(newValue);
-                  }}
-                />
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                disabled={isLoading}
-              >
-                {isLoading ? (
-                  <Box
-                    sx={{
-                      textAlign: "center",
-                      display: "flex",
-                      alignItems: "center",
-                      p: 0,
-                    }}
+          <Box sx={{ mt: 3 }}>
+            <Form submitHandler={onSubmit}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <FormTextField
+                    type="text-area"
+                    label="Review"
+                    rows={3}
+                    name="review"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography
+                    color="secondary"
+                    sx={{ mb: 1 }}
+                    variant="subtitle1"
                   >
-                    <CircularProgress
-                      size="20px"
+                    Give us your rating
+                  </Typography>
+                  <Rating
+                    precision={0.5}
+                    size="large"
+                    value={star}
+                    required
+                    onChange={(event, newValue) => {
+                      setStar(newValue);
+                    }}
+                  />
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <Box
                       sx={{
-                        color: "info.main",
+                        textAlign: "center",
+                        display: "flex",
+                        alignItems: "center",
+                        p: 0,
                       }}
-                    />
-                  </Box>
-                ) : (
-                  "Give Review"
-                )}
-              </Button>
-            </Grid>
+                    >
+                      <CircularProgress
+                        size="20px"
+                        sx={{
+                          color: "info.main",
+                        }}
+                      />
+                    </Box>
+                  ) : (
+                    "Give Review"
+                  )}
+                </Button>
+              </Grid>
+            </Form>
           </Box>
         </Box>
       </Container>
